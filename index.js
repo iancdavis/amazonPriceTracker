4a34646646cfd9c7ba37498bcccb34be7e67e73b
Ian Davis <iancdavis7@gmail.com>
2021-01-13 09:41:37 -0700

    price fetched

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const puppeteer = require('puppeteer')
 const $ = require('cheerio')
-const CronJob = require('cron').CronJob
-const nodemailer = require('nodemailer')
+//const CronJob = require('cron').CronJob
+//const nodemailer = require('nodemailer')
 
 const url = 'https://www.amazon.com/Sony-WH-1000XM4-Canceling-Headphones-phone-call/dp/B0863TXGM3/'
 
+async function configureBrowser() {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(url)
+    return page
+}
+
+
+async function checkPrice(page) {
+    
+    await page.reload()
+    let html = await page.evaluate(() => document.body.innerHTML)
+    //console.log(html)
+
+    $('#priceblock_ourprice', html).each(function() {
+        let dollarPrice = $(this).text()
+        console.log(dollarPrice)
+    })
+}
+
+async function monitor() {
+    let page = await configureBrowser()
+    await checkPrice(page)
+}
+
+monitor()
